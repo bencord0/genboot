8bda5fb8494085223fcaa9fe0036d110a7c800ce
Ben Cordero <bencord0@condi.me>
2013-11-30 05:00:39 +0000

    robustify(build_modules.sh)

--- build_modules.sh
+++ build_modules.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
-MAKEOPTS="-j$(grep processor /proc/cpuinfo|wc -l)"
 EMERGE_FLAGS="--buildpkg --getbinpkg --update --jobs --deep --newuse"
 
 emerge $EMERGE_FLAGS --usepkg  sys-kernel/aufs-sources
-cp config /usr/src/linux/.config
+test -f /usr/src/linux/.config || cp config /usr/src/linux/.config
 
 cd /usr/src/linux
 
 make olddefconfig
 make kvmconfig
-make $MAKEOPTS modules modules_install
+make modules
+make modules_install
 
