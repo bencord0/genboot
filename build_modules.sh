e7d88dec2827d4071f23f3952f925e0a303fb542
Ben Cordero (bencord0) <bencord0@condi.me>
2014-03-29 23:09:16 +0000

    All in one kernel image

--- build_modules.sh
+++ build_modules.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -x
 EMERGE_FLAGS="--buildpkg --getbinpkg --update --jobs --deep --newuse"
+MAKEOPTS="-j$(grep processor /proc/cpuinfo|wc -l)"
 
 emerge $EMERGE_FLAGS --usepkg  sys-kernel/aufs-sources
-test -f /usr/src/linux/.config || cp config /usr/src/linux/.config
+cp config.nosquash /usr/src/linux/.config
 
 cd /usr/src/linux
 
 make olddefconfig
 make kvmconfig
-make modules
+make $MAKEOPTS modules
 make modules_install
+make tarxz-pkg
+
+cp linux-"$(make kernelrelease)"-x86.tar.xz /root/linux-image.tar.xz
--- build_vmlinuz.sh
+++ build_vmlinuz.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
 MAKEOPTS="-j$(grep processor /proc/cpuinfo|wc -l)"
-
 INITRAMFS=/root/initramfs
+
 gunzip -c "${INITRAMFS}" > "${INITRAMFS}.cpio"
+cp config /usr/src/linux/.config
 
 cd /usr/src/linux
 
 make $MAKEOPTS targz-pkg
 
 cp "$(make image_name)" /root/vmlinuz
-cp linux-"$(make kernelrelease)"-x86.tar.gz /root/linux-image.tar.gz
