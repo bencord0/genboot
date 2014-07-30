ecd5b0e327fd252abce7ea2602ffdd5b5f4af682
Ben Cordero <bencord0@condi.me>
2014-07-30 08:51:18 +0100

    Unkeyword aufs-sources

--- build_modules.sh
+++ build_modules.sh
@@ -3,6 +3,7 @@ set -x
 EMERGE_FLAGS="--buildpkg --getbinpkg --update --jobs --deep --newuse"
 MAKEOPTS="-j$(grep processor /proc/cpuinfo|wc -l)"
 
+echo sys-kernel/aufs-sources >> /etc/portage/package.keywords/sys-kernel
 emerge $EMERGE_FLAGS --usepkg  sys-kernel/aufs-sources
 cp config.nosquash /usr/src/linux/.config
 
