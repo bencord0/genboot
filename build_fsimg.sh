544685d603a3956635061f659bf5f3897cce8f54
Ben Cordero (bencord0) <bencord0@condi.me>
2014-08-06 17:07:06 +0100

    Also build an AMI suitable root filesystem

---
+++ build_fsimg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+I=/root/rootfs.img
+M=mountpath
+
+rm -f "$I"
+truncate -s1G "$I"
+mkfs.ext4 "$I"
+
+mkdir -p "$M"
+mount -o loop "$I" "$M" || exit 1
+function clean_up () {
+    umount "$M"
+    rm -d "$M"
+}
+trap 'clean_up' EXIT
+
+unsquashfs -d "$M" -f /root/systemd.squashfs
