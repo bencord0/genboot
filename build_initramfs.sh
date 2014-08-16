644249181fb289074346c4a446f051dbedcd2c07
Ben Cordero (bencord0) <bencord0@condi.me>
2014-08-16 22:44:54 +0100

    Update initramfs building

--- build_initramfs.sh
+++ build_initramfs.sh
@@ -7,8 +7,8 @@ EMERGE_FLAGS="--buildpkg --getbinpkg --update --jobs --deep --newuse"
 eix -qI sys-kernel/dracut || emerge $EMERGE_FLAGS --usepkg --oneshot \
     sys-kernel/dracut
 
-dracut -f /root/initramfs $KVER -i /root/systemd.squashfs /root.squashfs 
-chmod a+r /root/initramfs
+dracut --no-compress -f /root/initramfs.cpio $KVER -i /root/systemd.squashfs /root.squashfs
+chmod a+r /root/initramfs.cpio
 
-dracut -f /root/initramfs.nosquash $KVER
+dracut --xz -f /root/initramfs.nosquash $KVER
 chmod a+r /root/initramfs.nosquash
--- build_vmlinuz.sh
+++ build_vmlinuz.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -x
 MAKEOPTS="-j$(grep processor /proc/cpuinfo|wc -l)"
-INITRAMFS=/root/initramfs
 
-gunzip -c "${INITRAMFS}" > "${INITRAMFS}.cpio"
 cp config /usr/src/linux/.config
 
 cd /usr/src/linux
