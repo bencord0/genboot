4fbfddb4331987fbccb26bb64a8ead2c557ac93d
Ben Cordero (bencord0) <bencord0@condi.me>
2014-08-18 16:04:53 +0100

    olddefconfig when building the kernel

--- build_vmlinuz.sh
+++ build_vmlinuz.sh
@@ -7,6 +7,7 @@ cp config /usr/src/linux/.config
 cd /usr/src/linux
 
 # Build a standalone kernel with bundled initramfs
+make olddefconfig
 make $MAKEOPTS bzImage
 
 cp arch/x86/boot/bzImage /root/vmlinuz
