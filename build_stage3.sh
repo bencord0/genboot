ac7cd9d7cc297ec0527f0fe90f957ac4be962b1e
Ben Cordero <bencord0@condi.me>
2014-11-26 16:35:49 +0000

    Allow nfs client mounts

--- build_stage3.sh
+++ build_stage3.sh
@@ -152,6 +152,10 @@ chown root chroot/var/empty
 ln -s '/usr/lib64/systemd/system/sshd.service' \
     'chroot/etc/systemd/system/multi-user.target.wants/sshd.service'
 
+# Allow NFS client mounts
+ln -s '/usr/lib64/systemd/system/rpc-mountd.service' \
+    'chroot/etc/systemd/system/multi-user.target.wants/rpc-mountd.service'
+
 rm -f /root/systemd.squashfs || true
 rm -f /root/stage3-systemd.tar.xz || true
 
