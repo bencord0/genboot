4c35263381815a0304efc1a7ec908bd1f4953032
Ben Cordero (bencord0) <bencord0@condi.me>
2015-05-01 01:18:38 +0100

    build_stage3: SYNC replaced with repos.conf

--- build_stage3.sh
+++ build_stage3.sh
@@ -30,10 +30,9 @@ rm -rf "chroot-prepare" "chroot"
 mkdir "chroot-prepare" "chroot"
 tar xavpf stage-template.tar.gz -C chroot-prepare && {
     grep MAKEOPTS /etc/portage/make.conf >> chroot-prepare/etc/portage/make.conf
-    grep SYNC /etc/portage/make.conf >> chroot-prepare/etc/portage/make.conf
     grep PORTAGE_BINHOST /etc/portage/make.conf >> chroot-prepare/etc/portage/make.conf
 
-    cp /etc/portage/repos.d/gentoo.conf chroot-prepare/etc/portage/repos.d/gentoo.conf
+    cp /etc/portage/repos.conf/gentoo.conf chroot-prepare/etc/portage/repos.conf/gentoo.conf
 }
 tar xavpf stage-template.tar.gz -C chroot
 
