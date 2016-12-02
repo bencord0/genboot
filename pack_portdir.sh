63a54a46170869285f1d21c21ff1a9726881fe67
Ben Cordero <bencord0@condi.me>
2016-12-02 22:26:04 +0000

    Repack portage tarball properly

--- pack_portdir.sh
+++ pack_portdir.sh
@@ -6,5 +6,5 @@ eclean-pkg &
 wait
 
 tar cf /root/portage.tar -C /usr portage/
-tar uf /root/portage.tar -C /var/lib/portage distfiles/ packages/
+tar uf /root/portage.tar -C /var/lib portage/distfiles/ portage/packages/
 xz /root/portage.tar
